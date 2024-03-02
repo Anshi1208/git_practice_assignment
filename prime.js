b14a49c1985780d3a2f67a03e771ee224b2f0c1a
Anshi1208 <Ankitasinghrajput5008@gmail.com>
2024-03-02 19:36:10 +0530

    solution

---
+++ prime.js
@@ -0,0 +1,16 @@
+function checkPrime(num) {
+    if (num <= 1) {
+        return false;
+    }
+    
+    for (let i = 2; i <= num / 2; i++) {
+        if (num % i === 1) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+console.log(checkPrime(5)); 
+console.log(checkPrime(9)); 
