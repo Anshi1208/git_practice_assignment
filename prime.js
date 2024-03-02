d3d6763a442bf64251225c397c7b4863384641c1
piyushsingh338 <piyushsingh639450@gmail.com>
2024-03-02 07:58:48 -0800

    resolve the conflix

--- prime.js
+++ prime.js
@@ -2,7 +2,7 @@ function checkPrime(num) {
     if (num <= 1) {
         return false;
     }
-    
+    // here i'll try tocorrect the code of prime number
     for (let i = 2; i <= num / 2; i++) {
         if (num % i === 0) {
             return false;
@@ -12,5 +12,5 @@ function checkPrime(num) {
     return true;
 }
 
-console.log(checkPrime(5)); 
-console.log(checkPrime(9)); 
+console.log(checkPrime(8)); 
+console.log(checkPrime(2)); 
